4a929d4435f044ad3e1305e27ae67149467775d8
PhHeinzenFurb <phheinzen@furb.br>
2025-05-28 11:08:51 -0300

    Atualizado bcb.data.R
    Criadas funções para gerar os dados do pacote GetBCBData e para o tratamento inicial padrão dos dados. Utilizados os codigos 29037, 4393, 27735, 20612, 20606.

--- bcb_data.R
+++ bcb_data.R
@@ -1,82 +1,73 @@
-# carregando as bibliotecas
+# chamando as biliotecas
 library(GetBCBData)
 library(tidyverse)
+
+# FUNÇÃO PARA EXTRAIR OS DADOS DO BCB VIA BIBLIOTECA GetBCBData
+get_bcb_data <- function(id, first_date){
+  gbcbd_get_series(
+    id = id,
+    first.date = first_date,
+    last.date = Sys.Date(),
+    format.data = "long",
+    use.memoise = TRUE,
+    cache.path = tempdir(),
+    do.parallel = FALSE
+  )
+}
+
+# FUNÇÃO PARA TRATAMENTO PADRÃO DOS DADOS GERADOS POR get_bcb_data
+df_bcb_format <- function(df){
+  df |>
+    select(-id.num) |>
+    pivot_wider(
+      names_from = series.name,
+      values_from = value
+    )
+}
 
 # CÓDIGO A SER UTILIZADO: 29037
 # Endividamento das famílias com o Sistema Financeiro Nacional 
 # em relação à renda acumulada dos últimos doze meses (RNDBF)
+
 my_id_29037 <- c("Endividamento das famílias %" = 29037)
 
-df_bcb_endiv <- gbcbd_get_series(
-  id = my_id_29037,
-  first.date = "2012-02-01",
-  last.date = Sys.Date(),
-  format.data = "long",
-  use.memoise = TRUE,
-  cache.path = tempdir(),
-  do.parallel = FALSE
-)
+df_endiv <- get_bcb_data(my_id_29037, "2012-02-01")
+df_endiv_format <- df_bcb_format(df_endiv)
 
-# CÓDIGO A SER UTILIZADO: 24381
-# Rendimento médio real efetivo de todos os trabalhos - PNADC
-my_id_24381 <- c("Rendimento médio real efetivo de todos os trabalhos R$" = 24381)
+# CÓDIGO A SER UTILIZADO: 4393
+# Índice de Confiança do Consumidor
 
-df_bcb_rend <- gbcbd_get_series(
-  id = my_id_24381,
-  first.date = "2012-02-01",
-  last.date = Sys.Date(),
-  format.data = "long",
-  use.memoise = TRUE,
-  cache.path = tempdir(),
-  do.parallel = FALSE
-)
+my_id_4393 <- c("Índice de Confiança do Consumidor" = 4393)
 
-# TRATANDO OS DADOS
-# 1. retirando a coluna "id.num"
-# 2. alterando o nome da coluna "values" para o valor em "series.name"
-df_bcb_endiv_alter <- df_bcb_endiv |>
-  select(-id.num) |> 
-  pivot_wider(
-    names_from = series.name,
-    values_from = value
-  ) 
+df_indice <- get_bcb_data(my_id_4393, "2012-02-01")
+df_indice_format <- df_bcb_format(df_indice)
 
-df_bcb_rend_alter <- df_bcb_rend |>
-  select(-id.num) |>
-  pivot_wider(
-    names_from = series.name,
-    values_from = value
-  )
+# CÓDIGO A SER UTILIZADO: 27735
+# Saldo das operações de crédito por atividade econômica 
+# - Geral - veículos automotores
 
-# JUNTANDO AS TABELAS df_bcb_endiv_alter E df_bcb_rend_alter (MERGE)
-df_bcb <- merge(df_bcb_endiv_alter, df_bcb_rend_alter)
+my_id_27735 <- c("Operações de Crédito - Veículos (R$ milhões)" = 27735)
 
-# TRATANDO OS DADOS 2
-# criando colunas para analises MoM e YoY
-df_bcb <- df_bcb |>
-  mutate(
-    `MoM Endividamento %`  = round(
-      ((`Endividamento das famílias %` - lag(`Endividamento das famílias %`)) / 
-         lag(`Endividamento das famílias %`)) * 100, digits = 2),
-    `YoY Endividamento %` = round(
-      ((`Endividamento das famílias %` - lag(`Endividamento das famílias %`, 12)) / 
-         lag(`Endividamento das famílias %`, 12)) * 100, digits = 2),
-    `MoM Renda Media %` = round(
-      ((`Rendimento médio real efetivo de todos os trabalhos R$`) - lag(`Rendimento médio real efetivo de todos os trabalhos R$`)) /
-         lag(`Rendimento médio real efetivo de todos os trabalhos R$`) * 100, digits = 2),
-    `YoY Renda Media %` = round(
-      ((`Rendimento médio real efetivo de todos os trabalhos R$`) - lag(`Rendimento médio real efetivo de todos os trabalhos R$`, 12)) /
-         lag(`Rendimento médio real efetivo de todos os trabalhos R$`, 12) * 100, digits = 2)
-    )
+df_cred_veic <- get_bcb_data(my_id_27735, "2012-02-01")
+df_cred_veic_format <- df_bcb_format(df_cred_veic)
 
-my_id_15892 <- c("Inadimplência das operações de crédito Sul PF %" = 15892)
+# CÓDIGO A SER UTILIZADO: 20612
+# Saldo da carteira de crédito com recursos direcionados 
+# - Pessoas físicas - Financiamento imobiliário total
 
-df_bcb_inad_sul <- gbcbd_get_series(
-  id = my_id_15892,
-  first.date = "2012-02-01",
-  last.date = Sys.Date(),
-  format.data = "long",
-  use.memoise = TRUE,
-  cache.path = tempdir(),
-  do.parallel = FALSE
-)
+my_id_20612 <- c("Operações de Crédito - Imobiliário (R$ milhões)" = 20612)
+
+df_cred_imob <- get_bcb_data(my_id_20612, "2012-02-01")
+df_cred_imob_format <- df_bcb_format(df_cred_imob)
+
+# CÓDIGO A SER UTILIZADO: 20606
+# Saldo da carteira de crédito com recursos direcionados 
+# - Pessoas físicas - Total
+
+my_id_20606<- c("Operações de Crédito - Total (R$ milhões)" = 20606)
+
+df_cred_total <- get_bcb_data(my_id_20606, "2012-02-01")
+df_cred_total_format <- df_bcb_format(df_cred_total)
+
+
+
